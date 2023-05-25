d78a10b4ca11e7b2dfe5cef7fd2e0dadc8b498bd
nicolas bourdin <nicolas_bourdin@hotmail.com>
2023-05-25 20:10:15 +0200

    en cours

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+require 'json'
+Movie.destroy_all
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)["results"]
+
+movies.each do |movie|
+  Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}"
+  )
+end
+
+puts "created #{Movie.count} movies !"
